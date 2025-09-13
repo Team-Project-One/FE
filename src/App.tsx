4560df4b029773b5952d05d7c5c312f7e24bbfee
Myungjun Jang <wkdaudns12@gmail.com>
2025-09-13 18:24:18 +0900

    feat: 여러 screen들의 네비게이트 연결

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import MainScreen from "./screens/MainScreen";
+import SignupLandingScreen from "./screen/SignupLandingScreen";
+import SignupBasicScreen from "./screen/SignupBasicScreen";
+import SignupDetailedScreen from "./screen/SignupDetailedScreen";
+import MainScreen from "./screen/MainScreen";
+import MyScreen from "./screen/MyScreen";
+import MatchingResultScreen from "./screen/MatchingResultScreen";
+import { Screen, NavigationHandler } from "./types";
 
 /**
  * 최상위 루트 컴포넌트
  * SafeAreaProvider로 앱을 감싸 SafeArea 값을 하위 컴포넌트에 제공
  */
 const App: React.FC = () => {
-  return (
-    <SafeAreaProvider>
-      <MainScreen />
-    </SafeAreaProvider>
-  );
+  const [currentScreen, setCurrentScreen] = useState<Screen>("signupLanding");
+
+  const handleNavigation: NavigationHandler = (screen) => {
+    setCurrentScreen(screen);
+  };
+
+  const renderScreen = () => {
+    switch (currentScreen) {
+      case "signupLanding":
+        return <SignupLandingScreen onNavigate={handleNavigation} />;
+      case "signupBasic":
+        return <SignupBasicScreen onNavigate={handleNavigation} />;
+      case "signupDetailed":
+        return <SignupDetailedScreen onNavigate={handleNavigation} />;
+      case "home":
+        return <MainScreen onNavigate={handleNavigation} />;
+      case "mypage":
+        return <MyScreen onNavigate={handleNavigation} />;
+      case "matchingResult":
+        return <MatchingResultScreen onNavigate={handleNavigation} />;
+      case "friends":
+        return <MainScreen onNavigate={handleNavigation} />; // 임시로 MainScreen 사용
+      default:
+        return <SignupLandingScreen onNavigate={handleNavigation} />;
+    }
+  };
+
+  return <SafeAreaProvider>{renderScreen()}</SafeAreaProvider>;
 };
 
 export default App;
